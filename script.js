1f22151d6443cd5b33bcadb4560def5d00bed9af
Coffee <gladiatorr128@gmail.com>
2025-04-09 23:39:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,3 +18,44 @@ $(document).ready(function () {
     },
   });
 });
+
+$(document).ready(function () {
+  $(".moviesSliderOne").owlCarousel({
+    loop: false,
+    margin: 10,
+    nav: false,
+    dots: false,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      767: {
+        items: 2,
+      },
+      992: {
+        items: 4,
+      },
+    },
+  });
+});
+
+$(document).ready(function () {
+  $(".moviesSliderTwo").owlCarousel({
+    loop: false,
+    margin: 10,
+    nav: true,
+    dots: false,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      767: {
+        items: 1,
+      },
+      992: {
+        items: 3,
+      },
+    },
+  });
+});
